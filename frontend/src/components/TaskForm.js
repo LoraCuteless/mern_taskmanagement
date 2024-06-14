9867bcd478692f2db9345918ceb2c19e529eaaac
LoraCutelesse <156152402+LoraCuteless@users.noreply.github.com>
2024-06-14 22:05:38 +0800

    Update task

---
+++ frontend/src/components/TaskForm.js
@@ -0,0 +1,66 @@
+import { useState } from "react"
+
+const TaskForm = () =>{
+    const [title, setTitle] = useState('')
+    const [load,setLoad ] = useState('')
+    const [reps, setReps] = useState('')
+    const [error, setError] = useState(null)
+
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+
+        const workout = {title, load, reps}
+
+        const response = await fetch('/api/workout',{
+            method:'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+        const json = await response.json()
+
+        if (!response.ok){
+            setError(json.error)
+        }
+        if (response.ok){
+            setTitle('')
+            setLoad('')
+            setReps('')
+            setError(null)
+            console.log('new task added', json)
+        }
+    }
+
+    return (
+      <form className="create" onSubmit={handleSubmit}>
+        <h3>Add a New Task</h3>
+        <label>Task Name:</label>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <label>Assigned:</label>
+        <input
+          type="text"
+          onChange={(e) => setLoad(e.target.value)}
+          value={load}
+        />
+
+        <label>No. of Task:</label>
+        <input
+          type="text"
+          onChange={(e) => setReps(e.target.value)}
+          value={reps}
+        />
+
+        <button>Add Task</button>
+        {error && <div className="error">{error}</div>}
+      </form>
+    );
+}
+
+export default TaskForm
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+
+import TaskForm from "../components/TaskForm"
+
 const Home =()=>{
+
+    const [workout, setWorkout] = useState(null)
+
+    useEffect(()=>{
+        const fetchWorks = async ()=>
+            {
+                const response = await fetch('/api/workout')
+                const json = await response.json()
+
+                if (response.ok){
+                    setWorkout(json)
+                }
+            }
+        fetchWorks()
+    },[])
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workout && workout.map((workouts)=>(
+                    <p key={workouts.id}>{workouts.title}</p>
+
+                ))}
+            </div>
+            <TaskForm />
         </div>
     )
 }
